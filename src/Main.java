4d03b829c891a4d22daa2761c823660274c6135a
Глеб <bul.gleb@gmail.com>
2022-09-02 13:45:44 +0400

    Homework_4_5 - Урок 2

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,66 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 1
-
-        int age = 19;
-        if (age >= 18) {
-            System.out.println("Поздравляем с совершеннолетием!");
-        }
-        else {
-            System.out.println("Ваш возраст совершеннолетия ещё не наступил, нужно немного подождать.");
-        }
-        // Задача 2
-
-        int age2 = 26;
-        if (age2 >= 2 && age2 <=6) {
-            System.out.println("Если возраст человека равен "+age2+", то ему нужно ходить в детский сад.");
-        }
-        if (age2 >= 7 && age2 <= 18) {
-            System.out.println("Если возраст человека равен "+age2+", то ему нужно ходить в школу.");
-        }
-        if (age2 > 18 && age2 <= 24) {
-            System.out.println("Если возраст человека равен "+age2+", то ему нужно ходить в университет.");
-        }
-        if (age2 > 24) {
-            System.out.println("Если возраст человека равен "+age2+", то ему пора пойти на работу.");
+        // Задача 1-2
+        // 0 = android; 1 = ios;
+        int clientOS = 1;
+        int clientDeviceYear = 2014;
+        if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
         // Задача 3
-
-        int trainCar = 23;
-        if (trainCar >= 1 && trainCar <=59) {
-            System.out.println("В вагоне есть сидячее место");
-        }
-        if (trainCar >= 60 && trainCar <= 101) {
-            System.out.println("В вагоне есть стоячее место.");
-        }
-        if (trainCar >= 102) {
-            System.out.println("В вагоне нет мест");
+        int year = 2024;
+        if (year % 100 == 0) {
+            System.out.println(year + " год не является високосным");
+        } else if (year % 400 == 0 || year % 4 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
         }
         // Задача 4
-
-        int age3 = 5;
-        if (age3 < 5) {
-            System.out.println("Если возраст ребёнка равен "+age3+", то он не может кататься на аттракционе.");
+        int deliveryDistance = 85;
+        int day;
+        if (deliveryDistance >= 20 && deliveryDistance <= 60) {
+            day = 2;
+        } else if (deliveryDistance > 60 && deliveryDistance < 100){
+            day = 3;
         }
-        if (age3 >= 5 && age3 <= 13) {
-            System.out.println("Если возраст ребёнка равен "+age3+", то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
+        else {
+            day = 1;
         }
-        if (age3 > 14) {
-            System.out.println("Если возраст ребёнка равен "+age3+", то он может кататься без сопровождения взрослого.");
-        }
+        System.out.println("Потребуется дней: " + day);
         // Задача 5
-
-        int one = 65;
-        int two = 43;
-        int free = 99;
-        if (free > two && free > one) {
-            System.out.println("free больше one и two");
-        }
-        if (two > one && two > free) {
-            System.out.println("two больше one и free");
-        }
-        if (one > two && one > free) {
-            System.out.println("one больше two и free");
+        int monthNumber = 11;
+        switch (monthNumber){
+            case 1:
+            case 2:
+                System.out.println("Принадлежит к сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Принадлежит к сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Принадлежит к сезону лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Принадлежит к сезону осень");
+                break;
+            case 12:
+                System.out.println("Принадлежит к сезону зима");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
         }
     }
 }
